e6ea5d2a55bdf6f720edfbaf8fe40248aebfb38e
hahey <heynaheyna9@gmail.com>
2020-11-12 15:00:30 +0100

    adapt liquid to hakyll for sidebar.html and add archives

---
+++ site.hs
@@ -0,0 +1,92 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList pageList) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/page.html" siteCtx
+            >>= loadAndApplyTemplate "templates/default.html" siteCtx
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    siteCtx
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =:
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home" `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/includes/sidebar.html" $ do
+        compile $ templateBodyCompiler
+            >>= applyAsTemplate sidebarCtx
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+siteCtx :: Context String
+siteCtx =
+    constField "baseurl" "" 'mappend'
+    constField "site_description" "Lanyon Theme on Hakyll" 'mappend'
+    constField "tagline" "A Fork of Lanyon based on Poole" 'mappend'
+    defaultContext
+
+pageList :: List
+pageList = ["about.rst", "contact.markdown"]
+
+sidebarCtx :: Context String
+sidebarCtx =
+    listField "list_pages" nodeCtx (return pageList) 'mappend'
+    constField "copy-year" "2020" 'mappend'
+    constField "github-repo" "https://github.com/hahey/lanyon-hakyll"
+
+nodeCtx :: Context String
+    urlField "node-url"
+    titleField "title"
