1269e507c5ed7e3f8e0f867ec6bbf92dd4630fb2
hahey <heynaheyna9@gmail.com>
2020-11-13 23:28:27 +0100

    initial working translation

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ---
 This theme is a fork of [Lanyon](https://github.com/poole/lanyon) based on [Poole](https://getpoole.com/)
 originated by Mark Otto (mdo).
+
+The pagination code is modified from [the code by Mike Limansky](https://github.com/limansky/limansky_me/blob/master/src/site.hs)
--- site.hs
+++ site.hs
@@ -2,6 +2,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Control.Monad (liftM)
+import qualified Data.Map as M
+import           System.FilePath               (takeBaseName)
 
 
 --------------------------------------------------------------------------------
@@ -15,18 +18,29 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList pageList) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/page.html" siteCtx
-            >>= loadAndApplyTemplate "templates/default.html" siteCtx
-            >>= relativizeUrls
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match (fromList $ map fromFilePath pageList) $ do
+        route $ setExtension "html"
+        compile $ do
+            pageName <- takeBaseName . toFilePath <$> getUnderlying
+            let pageCtx = constField pageName "" `mappend`
+                          siteCtx
+
+            pandocCompiler
+                >>= applyAsTemplate pageCtx
+                >>= loadAndApplyTemplate "templates/page.html"    pageCtx
+                >>= loadAndApplyTemplate "templates/default.html" pageCtx
+                >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/default.html" siteCtx
             >>= relativizeUrls
 
     create ["archive.html"] $ do
@@ -36,6 +50,7 @@ main = hakyll $ do
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Archives"            `mappend`
+                    constField "archive" "" `mappend`
                     siteCtx
 
             makeItem ""
@@ -43,51 +58,88 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
+    paginate <- buildPaginateWith postsGrouper "posts/*" postsPageId
 
-    match "index.html" $ do
+    paginateRules paginate $ \page pattern -> do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =:
+            posts <- recentFirst =<< loadAllSnapshots pattern "content"
+            let indexCtx =
+                    constField "title" (if page == 1 then "Home"
+                                                     else "Blog posts, page " ++ show page) `mappend`
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home" `mappend`
-                    defaultContext
+                    constField "home" "" `mappend`
+                    paginateContext paginate page `mappend`
+                    siteCtx
 
-            getResourceBody
+            makeItem ""
                 >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/index.html" indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
-    match "templates/includes/sidebar.html" $ do
-        compile $ templateBodyCompiler
-            >>= applyAsTemplate sidebarCtx
-
     match "templates/*" $ compile templateBodyCompiler
 
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend`
+                    bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+            renderAtom feedConfig feedCtx posts
 
 --------------------------------------------------------------------------------
+
+postsGrouper :: MonadMetadata m => [Identifier] -> m [[Identifier]]
+postsGrouper = liftM (paginateEvery 3) . sortRecentFirst
+
+postsPageId :: PageNumber -> Identifier
+postsPageId n = fromFilePath $ if (n == 1) then "index.html" else show n ++ "/index.html"
+
+--------------------------------------------------------------------------------
+
+feedConfig :: FeedConfiguration
+feedConfig = FeedConfiguration
+    { feedTitle       = "lanyon-hakyll: Lanyon Theme on Hakyll"
+    , feedDescription = "A Fork of Lanyon based on Poole"
+    , feedAuthorName  = "Heuna Kim"
+    , feedAuthorEmail = "ai@heuna-kim.net"
+    , feedRoot        = "https://github.com/hahey/lanyon-hakyll"
+    }
+
+--------------------------------------------------------------------------------
+
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
+    bodyField "post-body" `mappend`
     defaultContext
 
 siteCtx :: Context String
 siteCtx =
-    constField "baseurl" "" 'mappend'
-    constField "site_description" "Lanyon Theme on Hakyll" 'mappend'
-    constField "tagline" "A Fork of Lanyon based on Poole" 'mappend'
-    constField "site-title" "lanyon-hakyll"
+    constField "baseurl" "" `mappend`
+    constField "site_description" "Lanyon Theme on Hakyll" `mappend`
+    constField "tagline" "A Fork of Lanyon based on Poole" `mappend`
+    constField "site-title" "lanyon-hakyll" `mappend`
+    constField "copy-year" "2020" `mappend`
+    constField "github-repo" "https://github.com/hahey/lanyon-hakyll" `mappend`
+    sidebarCtx `mappend`
     defaultContext
 
-pageList :: List
-pageList = ["about.rst", "contact.markdown"]
+--------------------------------------------------------------------------------
+
+pageList = [("About.rst" :: String), ("Contact.markdown":: String)]
+pair = zip (map fromFilePath pageList) pageList
+pages :: [(Identifier, String)] -> [Item String]
+pages [] = []
+pages ((a,b):xs) = (Item a b):(pages xs)
 
 sidebarCtx :: Context String
 sidebarCtx =
-    listField "list_pages" nodeCtx (return pageList) 'mappend'
-    constField "copy-year" "2020" 'mappend'
-    constField "github-repo" "https://github.com/hahey/lanyon-hakyll"
+    listField "list_pages" nodeCtx (return $ pages pair) `mappend`
+    defaultContext
 
 nodeCtx :: Context String
-    urlField "node-url"
+nodeCtx =
+    urlField "node-url" `mappend`
     titleField "title"
